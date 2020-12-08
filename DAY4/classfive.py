f4d4c0d1df5de797ed0dcdfbc27696707de9171a
Ishuveera <50647154+Ishuveera@users.noreply.github.com>
2020-12-08 20:26:49 +0530

    Add files via upload

---
+++ DAY4/classfive.py
@@ -0,0 +1,27 @@
+class Num:
+    def __init__(self, value):        
+        self.var = 10
+        if type(value) == type(10):
+            self.var = value
+            
+    def __str__(self):
+        return str(self.var)
+        
+    def __add__(self, other):
+        return Num(self.var + other.var)
+
+    def __sub__(self, other):
+        return Num(self.var + other.var)    
+
+    def __gt__(self, other):
+         return self.var < other.var      
+        
+     
+var1 = Num(100)
+var2 = Num('some sting here')
+
+var3 = var1 + var2
+print(var1 < var2)
+print(f'var1--> {var1}')
+print(f'var2--> {var2}')
+print(f'var3--> {var3}')
